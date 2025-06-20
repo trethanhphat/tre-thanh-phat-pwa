14bcdc09b742d05c12aafeae31fc2e4479bf195d
duong273 <duong273@users.noreply.github.com>
2025-06-20 23:05:09 +0700

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+
+export default function Head() {
+  return (
+    <>
+      <title>My PWA App</title>
+      <meta name="description" content="A progressive web app built with Next.js" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/icons/icon-192.png" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  );
+}
