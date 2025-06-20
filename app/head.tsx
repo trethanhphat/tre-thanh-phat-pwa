559373a9577cbbe122414afbdedef9bbfc3917ad
duong273 <duong273@users.noreply.github.com>
2025-06-20 23:07:32 +0700

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,10 @@
 // app/head.tsx
-
+import { appName, appDescription, phone, email, copyright } from "@/libs/env";
 export default function Head() {
   return (
     <>
-      <title>My PWA App</title>
-      <meta name="description" content="A progressive web app built with Next.js" />
+      <title>{appName}</title>
+      <meta name="description" content="{appDescription}" />
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/icons/icon-192.png" />
       <meta name="theme-color" content="#000000" />
