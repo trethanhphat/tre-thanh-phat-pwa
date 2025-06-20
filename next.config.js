69d0cf1e88d29cacf1286e3a31b9b280d202bac1
duong273 <duong273@users.noreply.github.com>
2025-06-20 22:54:08 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 // ✅ Import plugin PWA
 const withPWA = require("next-pwa")({
-  dest: "public",
+  dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development", // 👈 chỉ bật PWA khi production
   buildExcludes: [/.*dynamic-css-manifest\.json$/], // 👈 loại bỏ file này khỏi precache
 });
-
-// ❌ Cấu hình cũ (chỉ có reactStrictMode)
-// module.exports = withPWA({
-//   reactStrictMode: true,
-// });
 
 // ✅ Cấu hình mới: bật App Router + giữ strict mode + PWA
 module.exports = withPWA({
