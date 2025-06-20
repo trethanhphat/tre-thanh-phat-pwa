3d124a495cb65834fbd3af0134a57c5448769588
duong273 <duong273@users.noreply.github.com>
2025-06-20 23:12:38 +0700

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
   return (
     <>
       <title>{appName}</title>
-      <meta name="description" content="{appDescription}" />
+      <meta name="description" content={appDescription} />
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/icons/icon-192.png" />
       <meta name="theme-color" content="#000000" />
