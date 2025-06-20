d3cdf21d9e39b671525f726687567b5c04fc9714
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-06-20 15:37:41 +0700

    Offline for app

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development", // 👈 chỉ bật PWA khi production
+  buildExcludes: [/.*dynamic-css-manifest\.json$/], // loại bỏ file _next/dynamic-css-manifest.json khỏi precache bằng cấu hình trong next.config.js.
 });
 
 module.exports = withPWA({
