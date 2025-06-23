2cfb9babb6c868c9e1b56302ae754f07cb380332
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-06-23 08:54:23 +0700

    apply font-ttp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import "./globals.css";
-import "@/src/fonts/ttp-font.css";
+import "@/src/fonts/font-ttp.css";
 import Head from "next/head";
 import Image from "next/image";
 import { appName, appDescription, phone, email, copyright } from "@/libs/env";
@@ -20,7 +20,8 @@ export default function RootLayout({ children }) {
           content="Giới thiệu doanh nghiệp Tre Thanh Phát - Hệ sinh thái ngành tre"
         />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#116530" />
+        <link rel="icon" href="/assets/icon/icon-192.png" type="image/png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <main style={{ padding: "1rem" }}>{children}</main>
