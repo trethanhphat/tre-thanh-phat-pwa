fa14a60920c248cf135e9642a753177a7b0b7a11
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-06-26 14:22:34 +0700

    Update

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -4,7 +4,7 @@ import { Product } from '@/types/product';
 
 export async function fetchProducts(): Promise<Product[]> {
   const res = await fetch(
-    `https://rungkhoai.com/wp-json/wc/v3/products?consumer_key=${process.env.NEXT_PUBLIC_RUNGKHOAI_CONSUMER_KEY}&consumer_secret=${process.env.NEXT_PUBLIC_RUNGKHOAI_CONSUMER_SECRET}`
+    `https://rungkhoai.com/wp-json/wc/v3/products?consumer_key=${process.env.RUNGKHOAI_CONSUMER_KEY}&consumer_secret=${process.env.RUNGKHOAI_CONSUMER_SECRET}`
   );
 
   if (!res.ok) {
