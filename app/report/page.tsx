4b3945c59191a07b7fe7a1bd188ad4f7ea408691
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-07-11 17:37:40 +0700

    className

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <h1 className="text-2xl font-bold mb-4">Báo cáo phát triển vùng trồng Tre Thanh Phát</h1>
           <div className="aspect-w-16 aspect-h-9">
             <a
-              class="inline-block px-4 py-2 bg-green-600 text-white rounded-xl no-underline mr-4"
+              className="inline-block px-4 py-2 bg-green-600 text-white rounded-xl no-underline mr-4"
               href="https://lookerstudio.google.com/reporting/470c0a0d-60ed-4191-bacb-46f02752fd88/page/kz9JF?s=kRSik9CXYew"
             >
               <button className="btn-secondary w-full py-4 text-lg bg-green-100 text-green-800 rounded-2xl border border-green-300">
