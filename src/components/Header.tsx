4b7bcc11e776c26867634f81f8a2f576500c33d9
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-07-17 11:35:30 +0700

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,8 +21,10 @@ export default function Header() {
   return (
     <>
       {/* Header cố định */}
-      <header className=`header-wrapper ${shrink ? 'shrink' : ''}`>
-        <a href={appUrl}></a><h1 className="font-ttp app-title">{appName}</h1></a>
+      <header className={`header-wrapper ${shrink ? 'shrink' : ''}`}>
+        <a href={appUrl}>
+          <h1 className="font-ttp app-title">{appName}</h1>
+        </a>
         {!shrink && <p className="app-description">{appDescription}</p>}
       </header>
     </>
