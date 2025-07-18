a96382be10c782b5e3f322535a7340e6c7812d8a
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-07-18 18:37:16 +0700

    .

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -19,6 +19,21 @@ export default function Home() {
           <div className="aspect-w-16 aspect-h-9">
             <a
               className="button"
+              href="https://lookerstudio.google.com/reporting/56850af2-7c18-4c89-b6e3-7290d72f9c89/page/QnqRF"
+            >
+              <button className="btn-secondary w-full py-4 text-lg bg-green-100 text-green-800 rounded-2xl border border-green-300">
+                📋 Xem báo cáo cây trồng
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="space-y-4">
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Báo cáo Tổng hợp</h2>
+          <div className="aspect-w-16 aspect-h-9">
+            <a
+              className="button"
               href="https://lookerstudio.google.com/reporting/de31c339-19fa-4080-8def-8d124c043394/page/hDqRF"
             >
               <button className="btn-secondary w-full py-4 text-lg bg-green-100 text-green-800 rounded-2xl border border-green-300">
