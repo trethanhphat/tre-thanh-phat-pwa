060487502ae51a40d834a58aa1ef7852b141589e
Tre Thanh Phát <duong273@users.noreply.github.com>
2025-07-23 17:39:53 +0700

    Update page.tsx

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -11,23 +11,11 @@ export default function Home() {
     <main className="min-h-screen bg-white text-gray-800 px-4 pt-6 pb-24">
       {/* Tiêu đề */}
       <h1 className="text-2xl font-bold mb-4">
-        Danh sách Báo cáo phát triển <span className="font-ttp">Tre Thanh Phát</span>
+        Danh sách Báo cáo phát triển của<span className="font-ttp">Tre Thanh Phát</span>
       </h1>
 
       {/* Các chức năng chính */}
-      <section className="space-y-4">
-        <div className="p-4">
-          <h2 className="text-2xl font-bold mb-4">Tổng số cây và diện tích đã trồng</h2>
-          <div className="aspect-w-16 aspect-h-9">
-            <a
-              className="button"
-              href="https://lookerstudio.google.com/reporting/56850af2-7c18-4c89-b6e3-7290d72f9c89/page/QnqRF"
-            >
-              📋 Xem báo cáo
-            </a>
-          </div>
-        </div>
-      </section>
+
       <section className="space-y-4">
         <div className="p-4">
           <h2 className="text-2xl font-bold mb-4">Số cây đã trồng từng tháng</h2>
@@ -49,6 +37,19 @@ export default function Home() {
             <a
               className="button"
               href="https://lookerstudio.google.com/reporting/470c0a0d-60ed-4191-bacb-46f02752fd88/page/kz9JF?s=kRSik9CXYew"
+            >
+              📋 Xem báo cáo
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="space-y-4">
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Tổng số cây và diện tích đã trồng</h2>
+          <div className="aspect-w-16 aspect-h-9">
+            <a
+              className="button"
+              href="https://lookerstudio.google.com/reporting/56850af2-7c18-4c89-b6e3-7290d72f9c89/page/QnqRF"
             >
               📋 Xem báo cáo
             </a>
