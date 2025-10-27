edf3cd0b3dfd55a69d5d3480f6059ddb67414c43
duong273 <duong273@users.noreply.github.com>
2025-10-27 17:28:59 +0700

    Update ProductsTable.tsx

--- app/products/ProductsTable.tsx
+++ app/products/ProductsTable.tsx
@@ -116,7 +116,7 @@ export default function ProductsTable({
                 }}
                 data-label="Ảnh sản phẩm"
               >
-                {console.log('[IMG SRC]', p.id, imageCache[p.id])}
+                {console.log(p.id, imageCache[p.id])}
                 {imageCache[p.id] ? (
                   <Link href={`/product/${p.id}`}>
                     <img src={imageCache[p.id]} alt={p.name} style={{ maxWidth: '150px' }} />
